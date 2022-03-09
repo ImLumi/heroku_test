e8dd09d286763a4faaa70349456c4b15cfd5d562
ImLumi <mikesimon150@gmail.com>
2022-03-09 21:45:23 +0100

    set port 80

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-const port = 4000;
+const port = 80;
 
 app.get('/', (req, res) => {
   res.json({res: "OK", message: "hello heroku cloud"});
